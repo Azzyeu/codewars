f70635cdaf034d2f58dc3bb0e88eda6a673e34bc
Azzyeu <98702053+Azzyeu@users.noreply.github.com>
2022-02-19 17:39:46 +0000

    first commit

--- 5 kyu/Prime Time.py
+++ 5 kyu/Prime Time.py
@@ -1,12 +1,3 @@
-def isPrime(n):
-    prime = True
-    for i in range(2, int(n**0.5)+1):
-        if n%i==0:
-            prime = False
-            return prime
-    else:
-        return prime
-
 def prime(n):
     primes = []
     if n < 2:
@@ -16,3 +7,12 @@ def prime(n):
             if isPrime(i):
                 primes.append(i)
         return primes
+
+def isPrime(n):
+    prime = True
+    for i in range(2, int(n**0.5)+1):
+        if n%i==0:
+            prime = False
+            return prime
+    else:
+        return prime
